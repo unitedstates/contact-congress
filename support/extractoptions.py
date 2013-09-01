fb59c99ed8868deb85ed1e24a1fd06dc33f4c6f7
makecakenotwar97@gmail.com <makecakenotwar97@gmail.com>
2013-09-01 19:06:11 -0400

    G000567 (fixes #357), fixed some hash issues, updated python file This one has campaign IDs as well as generic thank you message Python file now creates hash when option text does not match option value

--- support/extractoptions.py
+++ support/extractoptions.py
@@ -1,8 +1,8 @@
 '''
 HOW TO USE:
-  This file extracts the option "value" attributes.
+  This file extracts the option "value" attributes, and creates a hash if the option's text does not match the option's value
 
-  Copy this file to a separate location (so as not to hide random files
+  Copy this file to a separate location (so as not to send random files
     to github)
 
   Create a folder named "input" in the same location
@@ -27,20 +27,36 @@ import re
 YAMLselections = []
 
 #compile regular expressions
-select = re.compile(r'<select.*?</select>', re.DOTALL | re.IGNORECASE)
-selectoptions = re.compile(r'(?:<option.*?value\s*=\s*)(\".*?\")', re.IGNORECASE)
+#select = re.compile(r'<select.*?</select>', re.DOTALL | re.IGNORECASE)
+selectoptions = re.compile(r'(?:<option.*?value\s*=\s*\")(.*?)(?:\")', re.IGNORECASE)
+selecttext = re.compile(r'(?:<option.*?value.*?>)(.*?)(?:</)', re.IGNORECASE)
 
 for root,dirs,files in os.walk('.\input'):
     for file in files:
         with open('.\input\\' + file, 'r') as f:
             html = f.read()
-        selectors = re.findall(select, html)
-        for selector in selectors:
-            options = re.findall(selectoptions, selector)
-            data = '------------\n' + file + '\n' + 'options:' + '\n'
+        #selectors = re.findall(select, html)
+        #for selector in selectors:
+        options = re.findall(selectoptions, html)
+        text = re.findall(selecttext, html)
+        data = '------------\n' + file + '\n' + 'options:' + '\n'
+        if len(options) != len(text):
+            print("\nerror with text, printing option values only")
             for option in options:
                 data = data + "          "
-                data = data + "- " + option + '\n'
+                data = data + "- \"" + option + "\"\n"
             YAMLselections.append(data)
+        elif set(options) != set(text):
+            print("\noptions != text, printing hash")
+            for i in range(0,len(options)):
+                data = data + "          "
+                data = data + "\"" + text[i] + "\":" + " \"" + options[i] + "\"\n"
+            YAMLselections.append(data)
+        else:
+            for option in options:
+                data = data + "          "
+                data = data + "- \"" + option + "\"\n"
+            YAMLselections.append(data)
+            
 for x in YAMLselections:
     print(x)
