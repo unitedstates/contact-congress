432e954edb05958d7008761d9c87b4b4716dc3fd
Adam Greenhall <agreenhall@lyft.com>
2013-11-14 20:14:26 -0800

    add valid users to post-recieve hook

--- post-receive-hook.sh
+++ post-receive-hook.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 GIT_WORK_TREE=/root/call-congress git checkout -f
 cd /root/call-congress
+cp /root/valid_user_emails.txt data/users.txt
 # install requirments
 pip install -r requirements.txt
 # kill old app
