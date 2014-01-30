0d5d14d67e3e1f3575a25b1cddb9f5fdabd73f5d
Thomas Davis <thomasalwyndavis@gmail.com>
2014-01-30 16:21:05 +1000

    try count calls

--- models.py
+++ models.py
@@ -84,7 +84,7 @@ def log_call(db, params, campaign, request):
 
 def call_count(db):
     try:
-        return dict(count=db.session.query(func.Count(Call.zipcode)).all())
+        return dict(count=db.session.query(func.Count(Call.zipcode)).all()[0][0])
     except:
         return 0
 
